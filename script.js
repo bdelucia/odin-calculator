796575ff5856a47bcd76c8aa68c31bdcccef32fb
Robert DeLucia Jr. <bdelucia@asu.edu>
2024-11-20 16:48:00 -0700

    operate function

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+let operand1 = 0;
+let operator = "";
+let operand2 = 0;
+let result = 0;
+
 function add(a, b){ return a + b;}
 function subtract(a, b){ return a - b;}
 function multiply(a, b){ return a * b;}
 function divide(a, b){ return a / b;}
 
-console.log(add(1,5));
+function operate(operand1, operator, operand2){
+    // base case, check if inputs are valid
+    if(typeof(operand1) !== "number" || typeof(operator) !== "string" || typeof(operand2) !== "number")
+        return NaN;
+
+    switch (operator){
+        case '+':
+            result = add(operand1, operand2);
+            console.log(result);
+            break;
+        case '-':
+            result = subtract(operand1, operand2);
+            console.log(result);
+            break;
+        case '*': 
+            result = multiply(operand1, operand2);
+            console.log(result);
+            break;
+        case '/':
+            result = divide(operand1, operand2);
+            console.log(result);
+            break;
+    }
+}
+
+console.log(operate(1, '+', 3));
